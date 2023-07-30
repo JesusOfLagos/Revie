32763adb476662d9f44d042706a5163fd182e43e
Dot Dev <thejesusoflagos@gmail.com>
2023-07-30 20:37:55 +0000

    Added SignIn

--- Auth/signUp.vue
+++ Auth/signUp.vue
@@ -1,9 +1,13 @@
-<!-- SignIn.vue -->
+<!-- SignUp.vue -->
 <template>
   <div class="min-h-screen flex items-center justify-center bg-gray-100">
     <div class="max-w-md w-full px-6 py-8 bg-white rounded-lg shadow-md">
-      <h2 class="text-3xl font-semibold text-center">Sign In</h2>
+      <h2 class="text-3xl font-semibold text-center">Sign Up</h2>
       <form @submit.prevent="submitForm">
+        <div class="mt-4">
+          <label for="name" class="block text-gray-700 font-medium">Name</label>
+          <input v-model="name" type="text" id="name" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200">
+        </div>
         <div class="mt-4">
           <label for="email" class="block text-gray-700 font-medium">Email</label>
           <input v-model="email" type="email" id="email" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200">
@@ -13,11 +17,11 @@
           <input v-model="password" type="password" id="password" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200">
         </div>
         <div class="mt-6">
-          <button type="submit" class="w-full px-4 py-2 text-base font-semibold text-white bg-blue-600 rounded-md shadow-md hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-200">Sign In</button>
+          <button type="submit" class="w-full px-4 py-2 text-base font-semibold text-white bg-blue-600 rounded-md shadow-md hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-200">Sign Up</button>
         </div>
       </form>
       <div class="mt-4 text-center">
-        <p class="text-sm text-gray-600">Don't have an account? <router-link to="/signup" class="text-blue-600 font-semibold">Sign Up</router-link></p>
+        <p class="text-sm text-gray-600">Already have an account? <router-link to="/signin" class="text-blue-600 font-semibold">Sign In</router-link></p>
       </div>
     </div>
   </div>
@@ -27,14 +31,15 @@
 export default {
   data() {
     return {
+      name: "",
       email: "",
       password: "",
     };
   },
   methods: {
     submitForm() {
-      // You can add your sign-in logic here
-      console.log("Sign In form submitted!");
+      // You can add your sign-up logic here
+      console.log("Sign Up form submitted!");
     },
   },
 };
---
+++ LandingPage/about.vue
@@ -0,0 +1,18 @@
+<!-- About section -->
+<section class="py-16 bg-white">
+  <div class="container mx-auto">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+      <!-- About image -->
+      <div class="md:order-2">
+        <img src="about.jpg" alt="About Us" class="w-full rounded-lg shadow-lg">
+      </div>
+      <!-- About content -->
+      <div class="md:order-1">
+        <h2 class="text-3xl font-bold mb-4">About Us</h2>
+        <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, quam eu varius dictum, justo ex semper augue. Integer ut est ut orci pharetra bibendum id eu lorem. Etiam hendrerit dui id lorem pharetra feugiat. Donec ullamcorper dolor vitae nisl facilisis, vel viverra justo interdum.</p>
+        <p class="mb-6">Fusce at lorem eu velit ullamcorper sagittis non vel metus. Integer nec nisl suscipit, venenatis purus a, fringilla odio. Integer volutpat mauris felis, at facilisis nulla hendrerit eu. Phasellus at ipsum nulla. Fusce lobortis, metus ac dictum consectetur, lectus lectus feugiat tortor, eu bibendum turpis dolor at turpis. Ut facilisis euismod tincidunt.</p>
+        <a href="#contact" class="bg-blue-600 text-white py-2 px-6 rounded-full hover:bg-blue-700 transition-all duration-200">Contact Us</a>
+      </div>
+    </div>
+  </div>
+</section>
